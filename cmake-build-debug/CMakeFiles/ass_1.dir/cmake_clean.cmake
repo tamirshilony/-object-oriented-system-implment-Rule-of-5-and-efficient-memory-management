30c58e37121e4f085c66406640d49119c4d735ac
spl211 <shilonyt@post.bgu.ac.il>
2020-11-05 16:39:13 +0200

    first

---
+++ cmake-build-debug/CMakeFiles/ass_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass_1.dir/src/main.cpp.o"
+  "ass_1"
+  "ass_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
